b709881f1a89ca2e5c11a192c263e6e4d0e681f8
Tien Anh <anhmt@hitexglobal.com>
2024-01-21 23:54:03 +0700

    add next.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import dbConnect from '@/lib/monggodb'
-import { SupplierModel } from '@/models'
 import { Container } from './kit/Container'
 
 export default async function Page() {
-	await dbConnect()
-  SupplierModel.find()
 	return (
 		<h1 className='s1-default'>
 			<Container />
